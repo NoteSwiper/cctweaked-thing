ff84a8534de082b1db6cb15d09ec85ef1fc960e1
NoteSwiper <ryuuking0410@gmail.com>
2024-04-15 00:01:47 +0900

    Create noteswiperpkg.lua

---
+++ noteswiperpkg.lua
@@ -0,0 +1,21 @@
+return {
+  version = "i 0;i 0;i 0;pt 0;bi 1a88",
+  data_p = {
+    ["UUID_v1"] = "0b538896-fa6f-11ee-a951-0242ac120002",
+    ["UUID_v4"] = "4f3a1a36-0345-4abb-8f85-13ce968d7ce3",
+    ["UUID_v7"] = "018edd1a-b84c-728d-9242-daa08cc76928",
+    ["GUID_v1"] = "2ae28d51-f2b9-44ae-92b9-bfd4dc8dfe08",
+    ["Package"] = "me.noteswiper.cctweakedthing",
+    ["NtespID"] = "@38aa66dc288a-3fcad0-cctweaked_thing"
+  },
+  install_dir = "/ntesp-tools"
+  root_dir = "/"
+  extension = ".lua"
+  disk = "disk"
+  repo = {
+    GitHub = "https://raw.githubusercontent.com/NoteSwiper/cctweaked-thing/main/"
+  },
+  motds = {
+    "https://raw.githubusercontent.com/NoteSwuper/cctweaked-thing/main/motd.txt"
+  }
+}
